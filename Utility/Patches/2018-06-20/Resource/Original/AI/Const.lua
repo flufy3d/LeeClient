e2ed9ded69b851066bd5453e6d37ad5a54d7d6b2
CairoLee <cairoliyu@gmail.com>
2018-12-23 17:34:41 +0800

    提交 2018-06-20 版本所需要的 AI 文件

---
+++ Utility/Patches/2018-06-20/Resource/Original/AI/Const.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:54f229ccd1ff0bede7b726c837917ea040a9c7236e548cf2e655f98cdbd54d4a
+size 3242
---
+++ Utility/Patches/2018-06-20/Resource/Original/AI/Util.lua
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f35c563305c82c359d78f50649da26dffbcc435ac487809148628600301c8c7e
+size 2637
